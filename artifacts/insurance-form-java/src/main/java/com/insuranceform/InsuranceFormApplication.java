366b589d40becaa596c401e32bef04cf26be0522
360Alchemist <Info@360Alchemist.com>
2026-03-24 23:56:43 -0400

    Add Java Spring Boot insurance form: src/main/java/com/insuranceform/InsuranceFormApplication.java

---
+++ artifacts/insurance-form-java/src/main/java/com/insuranceform/InsuranceFormApplication.java
@@ -0,0 +1,11 @@
+package com.insuranceform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceFormApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceFormApplication.class, args);
+    }
+}
